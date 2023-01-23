caa0ed5e0ebde7a0ff60212bd01ecc03de7a9388
fivos-kanellis <fkanellis@gmail.com>
2023-01-23 03:35:01 +0000

    user summary tables

--- greenery/models/marts/intermediate/int_user_level_orders.sql
+++ greenery/models/marts/intermediate/int_user_level_orders.sql
@@ -13,25 +13,25 @@ SELECT
     ,min(o.created_at) as first_order_dt
     ,max(o.created_at) as last_order_dt
     ,max(case when o.status = 'delivered' then o.created_at end) as last_delivered_order_dt
-    ,max(case when o.order_status = 'shipped' then o.created_at end) as last_shipped_order_dt
+    ,max(case when o.status = 'shipped' then o.created_at end) as last_shipped_order_dt
     ,count(distinct o.order_id) as orders_total
-    ,count(distinct case when o.order_status = 'delivered' then o.order_id end) as orders_delivered
-    ,count(distinct case when o.order_status = 'shipped' then o.order_id end) as orders_shipped
-    ,count(distinct case when o.order_status = 'preparing' then o.order_id end) as orders_preparing
-    ,count(distinct case when o.order_status = 'shipped' and estimated_delivery_at < delivered_at then o.order_id end) as orders_shipped_est_late
-    ,count(distinct case when o.order_status = 'delivered' and estimated_delivery_at < delivered_at then o.order_id end) as orders_delivered_late
+    ,count(distinct case when o.status = 'delivered' then o.order_id end) as orders_delivered
+    ,count(distinct case when o.status = 'shipped' then o.order_id end) as orders_shipped
+    ,count(distinct case when o.status = 'preparing' then o.order_id end) as orders_preparing
+    ,count(distinct case when o.status = 'shipped' and estimated_delivery_at < delivered_at then o.order_id end) as orders_shipped_est_late
+    ,count(distinct case when o.status = 'delivered' and estimated_delivery_at < delivered_at then o.order_id end) as orders_delivered_late
     ,count(distinct case when o.shipping_service = 'ups' then o.order_id end) as orders_via_ups_total
-    ,count(distinct case when o.shipping_service = 'ups' and o.order_status = 'delivered' then o.order_id end) as orders_via_ups_delivered
-    ,count(distinct case when o.shipping_service = 'ups' and o.order_status = 'shipped' then o.order_id end) as orders_via_ups_shipped
+    ,count(distinct case when o.shipping_service = 'ups' and o.status = 'delivered' then o.order_id end) as orders_via_ups_delivered
+    ,count(distinct case when o.shipping_service = 'ups' and o.status = 'shipped' then o.order_id end) as orders_via_ups_shipped
     ,count(distinct case when o.shipping_service = 'fedex' then o.order_id end) as orders_via_fedex_total
-    ,count(distinct case when o.shipping_service = 'fedex' and o.order_status = 'delivered' then o.order_id end) as orders_via_fedex_delivered
-    ,count(distinct case when o.shipping_service = 'fedex' and o.order_status = 'shipped' then o.order_id end) as orders_via_fedex_shipped
+    ,count(distinct case when o.shipping_service = 'fedex' and o.status = 'delivered' then o.order_id end) as orders_via_fedex_delivered
+    ,count(distinct case when o.shipping_service = 'fedex' and o.status = 'shipped' then o.order_id end) as orders_via_fedex_shipped
     ,count(distinct case when o.shipping_service = 'usps' then o.order_id end) as orders_via_usps_total
-    ,count(distinct case when o.shipping_service = 'usps' and o.order_status = 'delivered' then o.order_id end) as orders_via_usps_delivered
-    ,count(distinct case when o.shipping_service = 'usps' and o.order_status = 'shipped' then o.order_id end) as orders_via_usps_shipped
+    ,count(distinct case when o.shipping_service = 'usps' and o.status = 'delivered' then o.order_id end) as orders_via_usps_delivered
+    ,count(distinct case when o.shipping_service = 'usps' and o.status = 'shipped' then o.order_id end) as orders_via_usps_shipped
     ,count(distinct case when o.shipping_service = 'dhl' then o.order_id end) as orders_via_dhl_total
-    ,count(distinct case when o.shipping_service = 'dhl' and o.order_status = 'delivered' then o.order_id end) as orders_via_dhl_delivered
-    ,count(distinct case when o.shipping_service = 'dhl' and o.order_status = 'shipped' then o.order_id end) as orders_via_dhl_shipped
+    ,count(distinct case when o.shipping_service = 'dhl' and o.status = 'delivered' then o.order_id end) as orders_via_dhl_delivered
+    ,count(distinct case when o.shipping_service = 'dhl' and o.status = 'shipped' then o.order_id end) as orders_via_dhl_shipped
     ,sum(o.order_cost) as total_order_cost_amt
     ,sum(o.shipping_cost) as total_shipping_cost_amt
     ,sum(o.order_total) as total_order_amt
